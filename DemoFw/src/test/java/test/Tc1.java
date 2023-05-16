c9b306a319c762121c4dd22acd45fad042213293
CHARAN <CHARAN@CHARAN>
2023-05-16 13:49:04 +0530

    First commit

---
+++ DemoFw/src/test/java/test/Tc1.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1 {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Tc1 passing");
+	}
+
+}
